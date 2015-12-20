9db4b9998f62c9da1a65b7a6dcfe8c945143b2fd
fengwanli <2533952444@qq.com>
2015-12-20 22:57:34 +0800

    Initial commit

---
+++ project1_HelloWorld/src/test/java/com/example/fengwanli/crs20120610110102fengwanli/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.fengwanli.crs20120610110102fengwanli;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
